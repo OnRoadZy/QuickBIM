a8304599fa6a6486fa38cc245a8582b38b74a5a3
OnRoadZy <amway_zhy@163.com>
2019-03-05 22:58:28 +0800

    2019.03.05更新。

---
+++ test/DrawInteractive/draw.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(require racket/gui)
+
+(provide paint)
+
+;回话及绘图===============================
+(define (paint cv dc)
+  (send dc set-scale 3 3)
+  (send cv set-canvas-background (make-object color% 0 0 0 1.0))
+  (send dc set-text-foreground "blue")
+  (send dc draw-text "文字显示（Show text）!" 0 0)
+  (send cv on-event (new mouse-event% [event-type (list 'left-down)]))
+  (if (send )
---
+++ test/DrawInteractive/interactive.rkt
@@ -0,0 +1,65 @@
+#lang racket
+
+(require racket/gui)
+(require "draw.rkt")
+
+(provide main-frame)
+
+;定义主界面=======================
+(define main-frame
+  (new frame%
+       [label "QuickBIM"]
+       [width 800]
+       [height 600]
+       [border 5]))
+
+;定义视图框架分割区域：========================
+;总容器：
+(define panel/all
+  (new vertical-pane%
+       [parent main-frame]
+       [alignment (list 'left 'top)]))
+
+;视图区：
+(define panel/view
+  (new horizontal-pane%
+       [parent panel/all]
+       [alignment (list 'left 'bottom)]))
+
+;画布：
+(define canvas
+  (new canvas%
+       [parent panel/view]
+       [style '(border)]
+       [paint-callback
+        (lambda (canvas dc) (paint canvas dc))]))
+
+;视图-交互间隔：
+(define panel/separator
+  (new vertical-pane%
+       [parent panel/all]
+       [alignment (list 'left 'bottom)]
+       [min-height 3]
+       [stretchable-height #f]))
+
+;交互区：
+(define panel/interactive
+  (new vertical-panel%
+       [parent panel/all]
+       [style '(border)]
+       [alignment (list 'left 'bottom)]
+       [stretchable-height #f]))
+
+;信息显示区：
+(define textfield/information
+  (new text-field% 
+       [parent panel/interactive]
+       [label #f]
+       [style '(multiple)]
+       [init-value "信息显示区……"]))
+
+;命令编辑区：
+(define textfield/command
+  (new text-field%
+       [parent panel/interactive]
+       [label "命令："]))
